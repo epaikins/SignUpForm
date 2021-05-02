007c846e02f2fccf82d3db74fbbb6f2b4c35e24a
epaikins <eugene.pokoo-aikins@amalitech.org>
2021-05-02 19:26:41 +0000

    Added validations to input fields

---
+++ scripts/script.js
@@ -0,0 +1,163 @@
+let username = document.querySelector('#username');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+let confirmPassword = document.querySelector('#confirmPassword');
+let person = document.getElementsByName("person");
+let form = document.querySelector(".inner");
+let selectRole = document.querySelector("h5");
+let signText = document.querySelector('h3');
+let personValue;
+
+
+function signUp() {
+    if(validate() && validatePassword()){
+        console.log(personValue, username.value, email.value, password.value, confirmPassword.value);
+        form.classList.add('hidden');
+        reset();
+    }
+    // reset();
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+
+function validatePassword() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if (password.value.match(lowerCaseLetters)) {
+        password.style.borderColor = "green";
+    } else {
+        password.style.borderColor = "red";
+        password.placeholder = "Invalid Password...";
+    }
+
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if (password.value.match(upperCaseLetters)) {
+        password.style.borderColor = "green";
+    } else {
+        password.style.borderColor = "red";
+        password.placeholder = "Invalid Password...";
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if (password.value.match(numbers)) {
+        password.style.borderColor = "green";
+    } else {
+        password.style.borderColor = "red";
+        password.placeholder = "Invalid Passwprd...";
+    }
+
+    // Validate length
+    if (password.value.length >= 6 && password.value.length <= 12) {} else {
+        password.style.borderColor = "red";
+        password.placeholder = "Invalid Password...";
+    }
+
+
+    // Validate Special Character
+    var characters = /[!@#$%^&*]/g;
+    if (password.value.match(characters)) {
+        password.style.borderColor = "green";
+    } else {
+        password.style.borderColor = "red";
+        password.placeholder = "Invalid Passwprd...";
+    }
+
+    if(password.value.match(characters) && password.value.length >= 6 && password.value.length <= 12 && password.value.match(numbers) && password.value.match(upperCaseLetters) && password.value.match(lowerCaseLetters)){
+        return true;
+    }
+
+
+    return false;
+}
+
+function validate() {
+    for (var i = 0; i < person.length; i++) {
+        if (person[i].checked) {
+            personValue = person[i].value;
+            break;
+        }
+    }
+
+    if (email.value === "" && username.value === "" && password.value === "" && personValue === undefined) {
+        email.style.borderColor = "red";
+        email.placeholder = "Enter Email...";
+
+        username.style.borderColor = "red";
+        username.placeholder = "Enter username...";
+
+        password.style.borderColor = "red";
+        password.placeholder = "Enter password...";
+
+        confirmPassword.style.borderColor = "red";
+        confirmPassword.placeholder = "Enter Password...";
+
+        selectRole.classList.remove('hidden');
+    } else if (!validateEmail(email.value) && username.value === "" && password.value !== confirmPassword) {
+        email.style.borderColor = "red";
+        email.placeholder = "Invalid Email...";
+
+        username.style.borderColor = "red";
+        username.placeholder = "Invalid Username...";
+
+        confirmPassword.style.borderColor = "red";
+        confirmPassword.placeholder = "Password must be the same";
+
+    }
+    else if(personValue === undefined){
+        selectRole.classList.remove('hidden');
+    }
+    else if (username.value === "") {
+        username.style.borderColor = "red";
+        username.placeholder = "Invalid Username...";
+    } else if (!validateEmail(email.value)) {
+        email.style.borderColor = "red";
+        email.placeholder = "Invalid Email...";
+    } else if (password.value !== confirmPassword.value) {
+        confirmPassword.style.borderColor = "red";
+        confirmPassword.placeholder = "Password must be the same...";
+    } else {
+        email.style.borderColor = "green";
+        username.style.borderColor = "green";
+        confirmPassword.borderColor - 'green';
+        return true;
+    }
+    return false;
+}
+
+function reset() {
+
+    for (var i = 0; i < person.length; i++)
+        person[i].checked = false;
+
+    username.value = '';
+    email.value = '';
+    password.value = '';
+    confirmPassword.value = '';
+}
+
+
+function facebook(){
+ signText.innerText = "Signing Up with facebook..."
+ form.classList.add("hidden");
+}
+
+function twitter(){
+    signText.innerText = "Signing Up with twitter..."
+    form.classList.add("hidden");
+   }
+
+   function google(){
+    signText.innerText = "Signing Up with google..."
+    form.classList.add("hidden");
+   }
+
+   function apple(){
+    signText.innerText = "Signing Up with apple..."
+    form.classList.add("hidden");
+   }
